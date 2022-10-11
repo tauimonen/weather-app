b2c85a8ea7f8e184df58c58b2f21446327d35f79
Toni Uimonen <tauimonen@gmail.com>
2022-10-11 11:49:49 +0300

    Add social media icons

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const wind = document.getElementById("wind");
 const humidity = document.getElementById("humidity");
 const weatherIcon = document.getElementById("weather-icon");
 const genDescription = document.querySelector(".weather");
+const inputField = document.getElementById("input");
 
 // Default location
 let input = "Helsinki";
@@ -16,17 +17,19 @@ readData(input);
 btn.addEventListener("click", function () {
   input = document.getElementById("input").value;
   readData(input);
+  inputField.value = "";
 });
 
 async function readData(location) {
   const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=682fdba59048a13bd9389478126db765`
+    `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=682fdba59048a13bd9389478126db765`,
+    { mode: "cors" }
   );
   const data = await response.json();
   displayLocation(data.name);
   displayTemp(data.main.temp);
   displayCountry(data.sys.country);
-  displayDescription("Weather now: " + data.weather[0].description);
+  displayDescription("Current weather: " + data.weather[0].description);
   displayGenDescription(data.weather[0].description);
   displayWind("Wind speed: " + data.wind.speed + " m/s");
   displayHumidity("Humidity: " + data.main.humidity);
@@ -36,11 +39,15 @@ async function readData(location) {
 const displayTemp = (t) => {
   const tempC = t - 273.15;
   const displayTemp = Math.round(tempC * 10) / 10;
-  
-  displayTemperature.innerHTML = displayTemp + "&deg;C";
+  displayTemperature.innerHTML = displayTemp + " &deg;C";
 };
 
 const displayLocation = (l) => {
+  if (l === undefined) {
+    alert(`Couldn't find the location "${input}"`);
+    inputField.value = "";
+    readData("Helsinki");
+  }
   locationDisplay.innerHTML = l;
 };
 
